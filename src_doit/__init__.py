64736b5e5dface8ffdfb3a0f067f86a5e0d0ff16
bozo.kopic <bozo@kopic.xyz>
2026-04-14 01:10:52 +0200

    requirements update

--- src_doit/__init__.py
+++ src_doit/__init__.py
@@ -7,7 +7,6 @@ from hat.doit.js import (ESLintConf,
                          run_eslint)
 from hat.doit.py import (get_task_build_wheel,
                          get_task_run_pytest,
-                         get_task_create_pip_requirements,
                          run_flake8)
 
 
@@ -20,8 +19,7 @@ __all__ = ['task_clean_all',
            'task_docs',
            'task_ts',
            'task_static',
-           'task_json_schema_repo',
-           'task_pip_requirements']
+           'task_json_schema_repo']
 
 
 build_dir = Path('build')
@@ -126,8 +124,3 @@ def task_json_schema_repo():
     """Generate JSON Schema Repository"""
     return common.get_task_json_schema_repo(schemas_json_dir.rglob('*.yaml'),
                                             json_schema_repo_path)
-
-
-def task_pip_requirements():
-    """Create pip requirements"""
-    return get_task_create_pip_requirements()
